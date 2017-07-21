d637f9114ff9aeeea9af43b910a3ae07cc5bfcf3
Ryan C. Thompson <rct@thompsonclan.org>
2017-07-21 13:19:43 -0700

    Rename to mac-pseudo-daemon
    OSX is now Mac OS, so this package's name should change as well.

--- mac-pseudo-daemon.el
+++ mac-pseudo-daemon.el
@@ -1,24 +1,24 @@
-;;; osx-pseudo-daemon.el --- Daemon mode that plays nice with OSX.
+;;; mac-pseudo-daemon.el --- Daemon mode that plays nice with Mac OS.
 
 ;; Author: Ryan C. Thompson
 ;; URL: https://github.com/DarwinAwardWinner/osx-pseudo-daemon
 ;; Version: 1.0
 ;; Created: 2013-09-20
-;; Keywords: convenience osx
+;; Keywords: convenience osx mac
 
 ;; This file is NOT part of GNU Emacs.
 
 ;;; Commentary:
 
-;; On OSX, if you use Cocoa Emacs' daemon mode and then close all GUI
-;; frames, the Emacs app on your dock becomes nonfunctional until you
-;; open a new GUI frame using emacsclient on the command line. This is
-;; obviously suboptimal. This package makes it so that whenever you
-;; close the last GUI frame, a new frame is created and the Emacs app
-;; is hidden, thus approximating the behvaior of daemon mode while
-;; keeping the Emacs dock icon functional. To actually quit instead of
-;; hiding Emacs, use CMD+Q (or Alt+Q if you swapped Alt & Command
-;; keys).
+;; On Mac OS, if you use Cocoa Emacs' daemon mode and then close all
+;; GUI frames, the Emacs app on your dock becomes nonfunctional until
+;; you open a new GUI frame using emacsclient on the command line.
+;; This is obviously suboptimal. This package makes it so that
+;; whenever you close the last GUI frame, a new frame is created and
+;; the Emacs app is hidden, thus approximating the behvaior of daemon
+;; mode while keeping the Emacs dock icon functional. To actually quit
+;; instead of hiding Emacs, use CMD+Q (or Alt+Q if you swapped Alt &
+;; Command keys).
 
 ;;; License:
 
@@ -42,31 +42,31 @@
 (require 'cl-lib)
 
 ;;;###autoload
-(defvar osxpd-mac-frame-types '(ns mac)
+(defvar macpd-mac-frame-types '(ns mac)
   "Set of frame types considered to be Mac GUI frames.")
 
 ;;;###autoload
-(defvar osxpd-mac-gui-features '(ns mac)
+(defvar macpd-mac-gui-features '(ns mac)
   "Set of features indicating Emacs is running a Mac GUI.")
 
 ;; Try to require mac gui features to ensure that `featurep' can find
 ;; them.
 ;;;###autoload
 (cl-loop
- for feature in osxpd-mac-gui-features
+ for feature in macpd-mac-gui-features
  do (require feature nil 'noerror))
 
 ;;;###autoload
-(defgroup osx-pseudo-daemon nil
-  "Emulate daemon mode in OSX by hiding Emacs when you kill the last GUI frame.")
+(defgroup mac-pseudo-daemon nil
+  "Emulate daemon mode in Mac OS by hiding Emacs when you kill the last GUI frame.")
 
-(defsubst osxpd-mac-gui-feature-provided ()
-  (cl-some #'featurep osxpd-mac-gui-features))
+(defsubst macpd-mac-gui-feature-is-provided ()
+  (cl-some #'featurep macpd-mac-gui-features))
 
-(defsubst osxpd-frame-is-mac-frame (frame)
-  (memq (framep frame) osxpd-mac-frame-types))
+(defsubst macpd-frame-is-mac-frame (frame)
+  (memq (framep frame) macpd-mac-frame-types))
 
-(defun osxpd-hide-emacs ()
+(defun macpd-hide-emacs ()
   "Hide all Emacs windows if running.
 
 This works for both `ns' and `mac' frame types."
@@ -81,46 +81,53 @@ This works for both `ns' and `mac' frame types."
       "-e" "end tell"))))
 
 ;;;###autoload
-(define-minor-mode osx-pseudo-daemon-mode
-  "Emulate daemon mode in OSX by hiding Emacs when you kill the last GUI frame.
+(define-minor-mode mac-pseudo-daemon-mode
+  "Hide Emacs instead of quitting when you kill the last frame on Mac OS.
 
-On OSX, if you use Cocoa Emacs' daemon mode and then close all
-GUI frames, the Emacs app on your dock becomes nonfunctional
-until you open a new GUI frame using emacsclient on the command
-line. This is obviously suboptimal. This package makes it so that
-whenever you close the last GUI frame, a new frame is created and
-the Emacs app is hidden, thus approximating the behvaior of
-daemon mode while keeping the Emacs dock icon functional. To
-actually quit instead of hiding Emacs, use CMD+Q (or Alt+Q if you
-swapped Alt & Command keys).
+On Mac OS, if you use Cocoa Emacs' real daemon mode and then
+close all GUI frames, the Emacs app on your dock becomes
+nonfunctional until you open a new GUI frame using emacsclient on
+the command line. This is obviously suboptimal. This package
+implements a fake daemon mode by making it so that whenever you
+close the last GUI frame, a new frame is created and the Emacs
+app is hidden (like pressing Cmd+H), thus approximating the
+behvaior of daemon mode while keeping the Emacs dock icon
+functional. This also approximates the behavior of document-based
+Mac applications, which can stay open even though there is no
+window.
 
-This mode has no effect unless Emacs is running on OSX with the
-Cocoa GUI, so it is safe to enable it unconditionally on all
-platforms."
-  :group 'osx-pseudo-daemon
+To actually quit Emacs instead of hiding it, use Cmd+Q (or
+Option+Q if you swapped the Option & Command keys).
+
+This mode has no effect unless Emacs is running on Mac OS, so if
+you have an Emacs config that is shared among multiple operating
+systems, it is safe to enable this mode unconditionally."
+  :group 'mac-pseudo-daemon
   :global t
-  ;; Enable by default on OSX
-  :init-value (osxpd-mac-gui-feature-provided))
+  ;; Enable by default on Mac OS
+  :init-value (macpd-mac-gui-feature-is-provided))
+(define-obsolete-function-alias 'osx-pseudo-daemon-mode 'mac-pseudo-daemon-mode "2017")
+(define-obsolete-variable-alias 'osx-pseudo-daemon-mode 'mac-pseudo-daemon-mode "2017")
 
-(defun osxpd-frame-is-last-mac-frame (frame)
+(defun macpd-frame-is-last-mac-frame (frame)
   "Returns t if FRAME is the only NS frame."
   (and
    ;; Mac frames supported
-   (osxpd-mac-gui-feature-provided)
+   (macpd-mac-gui-feature-is-provided)
    ;; Frame is mac frame
-   (osxpd-frame-is-mac-frame frame)
+   (macpd-frame-is-mac-frame frame)
    ;; No other frames on same terminal
    (<= (length (filtered-frame-list
                (lambda (frm) (eq (frame-terminal frm)
                             (frame-terminal frame)))))
       1)))
 
-(defun osxpd-keep-at-least-one-mac-frame (frame)
+(defun macpd-keep-at-least-one-mac-frame (frame)
   "If FRAME is the last GUI frame on Mac, open a new hidden frame.
 
 This is called immediately prior to FRAME being closed."
   (let ((frame (or frame (selected-frame))))
-    (when (osxpd-frame-is-last-mac-frame frame)
+    (when (macpd-frame-is-last-mac-frame frame)
       ;; If FRAME is fullscreen, un-fullscreen it.
       (when (eq (frame-parameter frame 'fullscreen)
                 'fullboth)
@@ -131,33 +138,33 @@ This is called immediately prior to FRAME being closed."
       (make-frame `(terminal ,(frame-terminal frame)))
       ;; Making a frame might unhide emacs, so hide again
       (sit-for 0.1)
-      (osxpd-hide-emacs))))
+      (macpd-hide-emacs))))
 
 ;; TODO: Is `delete-frame-hook' an appropriate place for this?
-(defadvice delete-frame (before osxpd-keep-at-least-one-mac-frame activate)
+(defadvice delete-frame (before macpd-keep-at-least-one-mac-frame activate)
   "When the last NS frame is deleted, create a new hidden one first."
-  (when osx-pseudo-daemon-mode
-    (osxpd-keep-at-least-one-mac-frame frame)))
+  (when mac-pseudo-daemon-mode
+    (macpd-keep-at-least-one-mac-frame frame)))
 
 ;; This is the function that gets called when you click the X button
 ;; on the window's title bar.
-(defadvice handle-delete-frame (around osxpd-never-quit-mac-emacs activate)
+(defadvice handle-delete-frame (around macpd-never-quit-mac-emacs activate)
   "Never invoke `save-buffers-kill-emacs' when deleting a Mac frame.
 
 Instead, just invoke `delete-frame' as normal. (Has no effect
-unless `osx-pseudo-daemon-mode' is active.)"
+unless `mac-pseudo-daemon-mode' is active.)"
   (let ((frame (posn-window (event-start event))))
-    (if (and osx-pseudo-daemon-mode
-             (osxpd-frame-is-mac-frame frame))
+    (if (and mac-pseudo-daemon-mode
+             (macpd-frame-is-mac-frame frame))
         (delete-frame frame t)
       ad-do-it)))
 
-(defadvice save-buffers-kill-terminal (around osx-pseudo-daemon activate)
+(defadvice save-buffers-kill-terminal (around mac-pseudo-daemon activate)
   "When killing an NS terminal, instead just delete all NS frames."
   (let ((frame (selected-frame)))
-    (if (and osx-pseudo-daemon-mode
-             (osxpd-frame-is-mac-frame frame))
-        ;; For NS GUI, just call `delete-frame' on each frame that's
+    (if (and mac-pseudo-daemon-mode
+             (macpd-frame-is-mac-frame frame))
+        ;; For Mac GUI, just call `delete-frame' on each frame that's
         ;; on the same terminal as the current frame. A new hidden one
         ;; will automatically be spawned by the advice to
         ;; `delete-frame' when the last existing frame is deleted.
@@ -167,6 +174,6 @@ unless `osx-pseudo-daemon-mode' is active.)"
                  (lambda (frm) (eq (frame-terminal frm) term)))))
       ad-do-it)))
 
-(provide 'osx-pseudo-daemon)
+(provide 'mac-pseudo-daemon)
 
-;;; osx-pseudo-daemon.el ends here
+;;; mac-pseudo-daemon.el ends here

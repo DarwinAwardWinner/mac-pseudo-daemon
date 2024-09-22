2003aed4831f17ada0598a49dcd9a63e6598fae3
Ryan C. Thompson <rct@thompsonclan.org>
2024-09-22 16:37:30 -0400

    Update setversion.sh script to handle dependencies as well

--- setversion.sh
+++ setversion.sh
@@ -4,8 +4,10 @@ TARGET_VERSION="$1"
 if [ -n "$TARGET_VERSION" ]; then
     echo "Updating version to $TARGET_VERSION";
     perl -i'orig_*' -lap \
-         -e "s/Version: [0-9.]+/Version: $TARGET_VERSION/g;" \
+         -e "s/;; Version: [0-9.]+/;; Version: $TARGET_VERSION/g;" \
+         -e "s/;; Package-Requires: (.*)pseudo-daemon \"[0-9.]+\"/;; Package-Requires: \$1pseudo-daemon \"$TARGET_VERSION\"/g;" \
          *.el
+    rm orig_*.el
 else
     echo "Usage: $0 VERSION_NUMBER"
 fi
